bebf6b0f510b03be219f56f37de65ef140ce1f51
YaroslavChmyr <yaroslav.ch72@gmail.com>
2024-12-26 17:59:50 +0200

    Fonts, assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="h1-bold">Welcome to the world of Next.js</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-space">Welcome to the world of Next.js</h1>
+    </>
+  );
 }
