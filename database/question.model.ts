235d204c4cc24740ccc5c7effc2f8d0f527fc4ec
YaroslavChmyr <yaroslav.ch72@gmail.com>
2025-06-17 19:04:19 +0300

    Add Mongoose models for Answer, Collection, Interaction, Question, Tag, TagQuestion, and Vote

---
+++ database/question.model.ts
@@ -0,0 +1,31 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IQuestion {
+  title: string;
+  content: string;
+  author: Types.ObjectId;
+  answers: number;
+  tags: Types.ObjectId[];
+  upvotes: number;
+  downvotes: number;
+  views: number;
+}
+
+const QuestionSchema = new Schema<IQuestion>(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    answers: { type: Number, default: 0 },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+    views: { type: Number, default: 0 },
+  },
+  { timestamps: true },
+);
+
+const Question =
+  models?.Question || model<IQuestion>("Question", QuestionSchema);
+
+export default Question;
