42357aa4c7eb2b9b0e60845565020047f54023a6
YaroslavChmyr <yaroslav.ch72@gmail.com>
2025-06-21 20:52:10 +0300

    chore: update Next.js to version 15.3.4 and add Pino logging library
    - Updated Next.js dependency from 15.1.2 to ^15.3.4 in package.json.
    - Added Pino and Pino-pretty for enhanced logging capabilities.
    - Created a new logger.ts file to configure and export a Pino logger instance with appropriate settings for production and development environments.

--- lib/handlers/error.ts
+++ lib/handlers/error.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { ZodError } from "zod";
 
 import { RequestError, ValidationError } from "../http-errors";
+import logger from "../logger";
 
 export type ResponseType = "api" | "server";
 
@@ -28,6 +29,10 @@ const formatResponse = (
 
 const handleError = (error: unknown, responseType: ResponseType = "server") => {
   if (error instanceof RequestError) {
+    logger.error(
+      { err: error },
+      `${responseType.toUpperCase()} Error: ${error.message}`,
+    );
     return formatResponse(
       responseType,
       error.statusCode,
@@ -40,6 +45,11 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
       error.flatten().fieldErrors as Record<string, string[]>,
     );
 
+    logger.error(
+      { err: error },
+      `${responseType.toUpperCase()} Validation Error: ${validationError.message}`,
+    );
+
     return formatResponse(
       responseType,
       validationError.statusCode,
@@ -49,13 +59,14 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
   }
 
   if (error instanceof Error) {
+    logger.error(error.message);
     return formatResponse(
       responseType,
       500,
       error.message || "An unexpected error occurred.",
     );
   }
-
+  logger.error({ err: error }, "An unexpected error occurred:");
   return formatResponse(responseType, 500, "An unexpected error occurred.");
 };
 
