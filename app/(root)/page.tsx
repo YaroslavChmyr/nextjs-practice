af80d5273e2d4ad5ba5b40655af29d1bc3770067
YaroslavChmyr <yaroslav.ch72@gmail.com>
2025-08-02 15:19:41 +0300

    Implement user authentication features: add UserAvatar component, enhance navigation with sign-out functionality, and update session handling in navbar and sidebar.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 
-import { auth } from "@/auth";
 import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
@@ -55,10 +54,6 @@ interface SearchParams {
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  const session = await auth();
-
-  console.log("Session:", session);
-
   const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
